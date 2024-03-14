bf1b0c5f5bddfdea46ae9282463b6f20d488a63a
binod2065 <sarma.binod@gmail.com>
2024-03-14 12:26:10 +0400

    changes

--- src/db/config.js
+++ src/db/config.js
@@ -1,1 +1,10 @@
-
+/** @type {import ('@sap/hana-client').ConnectionOptions;} */
+export default  {
+    serverNode: '10.0.0.5:30013',
+    uID: 'SAP',
+    pwd: 'VOOS@lnx#1239',
+    database: 'VOS',
+    currentSchema : "VOOS_LIVE",
+  };
+  
+  
